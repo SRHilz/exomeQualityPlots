0cc769bf569dd5cb9d0ff60968d7ad5b75a196a0
Stephanie Hilz <stephanie.hilz@gmail.com>
2017-12-08 21:15:01 -0800

    added in option to specify bam path

--- get_coverage.sh
+++ get_coverage.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/bash                                                                                                                                                                                                        
 cd $PBS_O_WORKDIR
-bampath="/costellolab/jocostello/LG3/exomes_recal/${patient}/" 
-#bampath="/home/shilz/data/exome2_recal/${patient}/"
+bampath="/costellolab/jocostello/LG3/exomes_recal/${patient}/"
+#bampath="/home/shilz/data/exome2_recal/${patient}/"                                                                                                                                                               
 for entry in $bampath*.bam
 do
     output=$(echo "$entry" | sed "s|$bampath||g").hist
-    bedtools coverage -hist -b $entry -a /home/shilz/database/nimblegen3/SeqCap_EZ_Exome_v3_hg19_capture_targets.bed | grep ^all > $output 
+    bedtools coverage -hist -b $entry -a $bedpath | grep ^all > $output
     done
